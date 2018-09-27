036e8ac94758e58a1e383630ce9b13eecd5256b3
hyazinthe <hyazinthe@gmail.com>
2018-09-27 23:37:40 +0900

    vuex router added

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <h2>small change</h2>
-    <h2>Change in develp branch</h2>
-    <h3>DEV BRANCH</h3>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
